dbf69b041200fed4c582178b61603898c6bc2e31
VISHNURANJAN V D <148265392+VDVR@users.noreply.github.com>
2024-07-30 19:59:33 +0530

    Update CommandFactory.java

--- Rocket_Launch_Simulator/Rocket_Launch_Simulator/src/rocket/commands/CommandFactory.java
+++ Rocket_Launch_Simulator/Rocket_Launch_Simulator/src/rocket/commands/CommandFactory.java
@@ -1,10 +1,10 @@
 package rocket.commands;
 
 public class CommandFactory {
-    // Factory method to create a Command object from a given input string
+    
     public static Command createCommand(String input) {
 
-        // Create and return a new Command object with the specified input string
+        
         return new Command(input);
         
     }
